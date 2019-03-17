dde96851c7c8fcf2c5302e6be4c14a8d32bb839f
Alyssa Brown <alyssa.brown@live.com>
2019-03-17 17:01:48 +0000

    Automatically backed up by Learn

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -4,9 +4,17 @@ def prying_into_the_method
 	inside_the_method = "We're inside the method"
 	puts inside_the_method
 	puts "We're about to stop because of pry!"
-	binding.pry
+
 	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
 	puts this_variable_hasnt_been_interpreted_yet
 end
+
+def self.scrape_hike_one
+   doc = Nokogiri::HTML (open("https://dayhikesneardenver.com/bear-canyon-loop-hike-ncar/"))
+   name = doc.search("h1.post entry-title").text 
+   location = doc.css("td.snapshotdetail").text 
+   distance = doc.css("td.snapshotdetail").text 
+    binding.pry 
+  end 
 
 prying_into_the_method
